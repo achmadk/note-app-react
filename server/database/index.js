60326b096a0f3a598d18a21c64376908e3603853
Achmad Kurnianto <achmad.kurnianto@gmail.com>
2017-09-27 12:35:36 +0700

    add `useMongoClient` option for connect to MongoDB

--- server/database/index.js
+++ server/database/index.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose'),
 addDummyData = require('./queries/add-dummy-data')
 initDatabase = () => {
   mongoose.Promise = global.Promise
-  mongoose.connect('mongodb://127.0.0.1/note-app-react')
-    .then(() => {
+  mongoose.connect('mongodb://127.0.0.1/note-app-react', {
+    useMongoClient: true
+  }).then(() => {
       console.log('connection to database succesful')
       addDummyData()
     })
